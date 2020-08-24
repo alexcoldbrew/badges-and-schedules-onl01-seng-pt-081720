fa3c0e740dca7eef13cf104229e6701edfb6a72e
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-24 21:02:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,5 +25,7 @@ def printer(attendees)
   attendees.each do |badge|
    puts "Hello, my name is #{badge}."
   end
-  
+  attendees.each_with_index do |attendee, index|
+    puts "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  end
 end
