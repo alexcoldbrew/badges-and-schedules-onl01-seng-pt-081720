f54317447dfa6e1bb558b4900542642e7c6353c5
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-24 20:47:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(attendees)
   array = []
-  attendees.each_with_index do |attendee, index + 1|
+  attendees.each_with_index do |attendee, index|
     array << "Hello, #{attendee}! You'll be assigned to room #{index}!"
   end
   array
